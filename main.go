c20bb01aaa1559a874c6886fdd9923f7bf56d31b
dtmu <dtloveum@gmail.com>
2020-03-20 21:55:27 +0900

    adopting goroutin

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/urfave/cli"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
@@ -18,35 +21,64 @@ func main() {
 }
 
 func action(c *cli.Context) error {
-	ip, err := getFromMyglobalip()
-	if err == nil {
-		fmt.Println(ip)
-		return nil
+	ip := make(chan string, 1)
+	go getFromInet(ip)
+	go getFromMyglobalip(ip)
+	go getFromCman(ip)
+	go timeOut(ip)
+	r := <-ip
+	if r == "timeout" {
+		em := "You cannot get global ip address."
+		fmt.Println(em)
+		return errors.New(em)
 	}
-	ip, err = getFromCman()
-	if err == nil {
-		fmt.Println(ip)
-		return nil
-	}
-	em := "You cannot get global ip address."
-	fmt.Println(em)
-	return errors.New(em)
+	fmt.Println(r)
+	return nil
 }
 
-func getFromMyglobalip() (string, error) {
+func getFromMyglobalip(ip chan string) {
 	doc, err := goquery.NewDocument("https://www.myglobalip.com/")
 	if err != nil {
-		return "", err
+		return
 	}
-	ip := doc.Find("h3#default > span.ip").Text()
-	return ip, nil
+	ip <- doc.Find("h3#default > span.ip").Text()
+	close(ip)
 }
 
-func getFromCman() (string, error) {
+func getFromCman(ip chan string) {
 	doc, err := goquery.NewDocument("https://www.cman.jp/network/support/go_access.cgi")
 	if err != nil {
-		return "", err
+		return
 	}
-	ip := doc.Find("div.inArea > div.outIp").Text()
-	return ip, nil
+	ip <- doc.Find("div.inArea > div.outIp").Text()
+	close(ip)
+}
+
+func getFromInet(ip chan string) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://inet-ip.info/", nil)
+	if err != nil {
+		return
+	}
+	req.Header.Set("User-Agent", `curl/7.58.0`)
+	if err != nil {
+		return
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return
+	}
+	ip <- string(b[:len(b)-2])
+	close(ip)
+}
+
+// timeout it 60 seconds
+func timeOut(ip chan string) {
+	time.Sleep(time.Second * 60)
+	ip <- "timeout"
+	close(ip)
 }
